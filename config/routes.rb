3018f4f0e4cadb478543576be9d56c686e0844eb
mazeneldeeb2 <mazeneldeeb9@gmail.com>
2023-05-30 15:10:10 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'user/registrations' }
+  resources :users, only: [:show]
+  resources :user_stocks, except: [:edit, :update, :show]
+  resources :friendships
+
+  root to: 'users#my_portfolio'
+
+  get 'my_portfolio',   to: 'users#my_portfolio'
+  get 'search_stocks',  to: 'stocks#search'
+  get 'my_friends',     to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend',    to: 'users#add_friend'
+end
---
+++ db/migrate/20171119033039_create_user_stocks.rb
@@ -0,0 +1,10 @@
+class CreateUserStocks < ActiveRecord::Migration
+  def change
+    create_table :user_stocks do |t|
+      t.integer :user_id, index: true, foreign_key: true
+      t.integer :stock_id, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
